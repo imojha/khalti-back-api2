cd16e0fbd98619a104e6f2f673e14419d5aee59b
imojha <pas074bct044@wrc.edu.np>
2023-07-23 17:19:36 +0545

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello there!")
+})
+
+app.post("/api/khalti-payment", async (req, res) => {
+    const payload = req.body;
+    const khaltiResponse = await axios.post(
+        "https://a.khalti.com/api/v2/epayment/initiate/",
+        payload,
+        {
+            headers: {
+                Authorization: "Key 6553308be8f54147930bb0b64858c5e4",
+            }
+        }
+    );
+
+    if (khaltiResponse) {
+
+        return res.json({
+            success: true,
+            data: khaltiResponse.data
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'something went wrong'
+        })
+    }
+
+
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
